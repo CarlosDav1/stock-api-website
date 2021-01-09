8d82b862e5e462268e926e834da2a9b18475bdd5
CarlosDav1 <carlosdav1@outlook.com>
2021-01-09 08:04:40 -0700

    Optimized the script

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function DefineStock(stockSymbol){
     let stockURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stockSymbol + "&interval=5min&outputsize=compact&apikey=8A2FATO31TXMHYZQ";
     let dailyURL = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol="+ stockSymbol +"&apikey=8A2FATO31TXMHYZQ";
     let yearlyURL = "https://www.alphavantage.co/query?function=OVERVIEW&symbol="+ stockSymbol +"&apikey=8A2FATO31TXMHYZQ";
-
+    
     //Call the following function whenever I want to check the data for a Stock
     async function GetApi(url) { 
         const response = await fetch(url); 
@@ -23,37 +23,24 @@ function DefineStock(stockSymbol){
 
     async function GetStockValue(){
         let data = await GetApi(stockURL);
-
-        //Changing the data inside the respective HTML element
-        document.getElementById("stockHeading").innerHTML = data["Meta Data"]["2. Symbol"];
-        for(x in data["Time Series (5min)"]){
-            let latestPrice = data["Time Series (5min)"][x]["4. close"]; //Getting the most recent price
-            let shortestPrice = latestPrice.slice(0, latestPrice.indexOf(".") + 3); //slicing the price so it only has 2 decimals
-            document.getElementById("current").innerHTML = shortestPrice;
-            break;
-        }
-    }
-
-    async function HighAndLows(){
         let dailyData = await GetApi(dailyURL);
         let weeklyData = await GetApi(yearlyURL);
+
+        let currentPrice;
 
         let highLowDaily = GetHighLow(dailyData["Global Quote"]["03. high"], dailyData["Global Quote"][ "04. low"]);
         let highLowWeekly = GetHighLow(weeklyData["52WeekHigh"], weeklyData["52WeekLow"]);
 
-        document.getElementById("24Max").innerHTML = highLowDaily[0];
-        document.getElementById("24Min").innerHTML = highLowDaily[1];
-        document.getElementById("yMax").innerHTML = highLowWeekly[0];
-        document.getElementById("yMin").innerHTML = highLowWeekly[1];
-    }
-    
-    GetStockValue();
-    HighAndLows();
+        for(x in data["Time Series (5min)"]){
+            let latestPrice = data["Time Series (5min)"][x]["4. close"]; //Getting the most recent price
+            currentPrice = latestPrice.slice(0, latestPrice.indexOf(".") + 3);
+            break;
+        }
 
-    //TODO: tomar el input del usuario para lo siguiente
-    //let probability =  ProbabilityCalculation(currentStockValue, 300, 250);
-    //document.getElementById("maxPercentage").innerHTML = probability[0];
-    //document.getElementById("minPercentage").innerHTML = probability[1];
+        ElementUpdate([data["Meta Data"]["2. Symbol"], currentPrice, highLowDaily, highLowWeekly]);
+    }
+
+    GetStockValue();
 }
 
 //The following code gives every button on the upper left of the site an event listener 
@@ -70,8 +57,25 @@ function ProbabilityCalculation(price, possibleHigh, possibleLow){
    return [probabilityToHigh, probabilityToLow];
 }
 
+function ElementUpdate(info){
+    document.getElementById("stockHeading").innerHTML = info[0];
+
+    document.getElementById("current").innerHTML = info[1];
+
+    document.getElementById("24Max").innerHTML = info[2][0];
+    document.getElementById("24Min").innerHTML = info[2][1];
+    document.getElementById("yMax").innerHTML = info[3][0];
+    document.getElementById("yMin").innerHTML = info[3][1];
+
+    //TODO: tomar el input del usuario para lo siguiente
+    //let probability =  ProbabilityCalculation(currentStockValue, 300, 250);
+    //document.getElementById("maxPercentage").innerHTML = probability[0];
+    //document.getElementById("minPercentage").innerHTML = probability[1];
+}
+
+
 //TODO: Quitar el comentario una vez que esté lista la grafica
-//DefineStock("FB");
+DefineStock("FB");
 
 
 
